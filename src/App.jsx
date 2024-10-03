2cdad39f4764688d383f6bb48b58b03f9e55bfd6
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2024-10-03 23:11:52 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import {TodoWrapper} from "./components/TodoWrapper.jsx";
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+    <TodoWrapper />
+    </div>
+    </>
+  );
+}
+
+export default App;
